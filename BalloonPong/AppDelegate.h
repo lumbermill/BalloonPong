c6f4178d339674f9e51e4f419aa04003528688aa
Yosei Ito <y-itou@lumber-mill.co.jp>
2013-11-13 13:11:13 +0900

    Initial Commit

---
+++ BalloonPong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BalloonPong
+//
+//  Created by Itou Yousei on 11/13/13.
+//  Copyright (c) 2013 LumberMill, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BalloonPong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BalloonPong
+//
+//  Created by Itou Yousei on 11/13/13.
+//  Copyright (c) 2013 LumberMill, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
